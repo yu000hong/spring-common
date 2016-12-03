f81143ce0fe9c2ce8b02ceaacd311eddeeae484e
余红 <yuhong4@staff.weibo.com>
2016-12-03 23:20:53 +0800

    remove comments

--- src/main/groovy/com/github/yu000hong/spring/common/redis/RedisHashUtilEditor.groovy
+++ src/main/groovy/com/github/yu000hong/spring/common/redis/RedisHashUtilEditor.groovy
@@ -1,17 +1,17 @@
-//package com.github.yu000hong.spring.common.redis
-//
-//import org.springframework.data.redis.core.RedisOperations
-//
-//import java.beans.PropertyEditorSupport
-//
-//class RedisHashUtilEditor extends PropertyEditorSupport {
-//
-//    public void setValue(Object value) {
-//        if (value instanceof RedisOperations) {
-//            super.setValue(new RedisHashUtil((RedisOperations) value))
-//        } else {
-//            throw new IllegalArgumentException('Editor supports only conversion of type ' + RedisOperations.class)
-//        }
-//    }
-//
-//}
+package com.github.yu000hong.spring.common.redis
+
+import org.springframework.data.redis.core.RedisOperations
+
+import java.beans.PropertyEditorSupport
+
+class RedisHashUtilEditor extends PropertyEditorSupport {
+
+    public void setValue(Object value) {
+        if (value instanceof RedisOperations) {
+            super.setValue(new RedisHashUtil((RedisOperations) value))
+        } else {
+            throw new IllegalArgumentException('Editor supports only conversion of type ' + RedisOperations.class)
+        }
+    }
+
+}
